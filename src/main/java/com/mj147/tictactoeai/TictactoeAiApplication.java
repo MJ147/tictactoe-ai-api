68341c7bb373353f641dbbef48770ff09ecf7d2c
Marek Jaczewski <jaczewski147@gmail.com>
2020-06-29 21:58:20 +0200

    initial commit

---
+++ src/main/java/com/mj147/tictactoeai/TictactoeAiApplication.java
@@ -0,0 +1,13 @@
+package com.mj147.tictactoeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mj147/tictactoeai/TictactoeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj147.tictactoeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
